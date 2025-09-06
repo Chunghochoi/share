603d3a3e6bc6585d33f7292da72e8fe6bb7e7e2c
chungg <chungdayy2006@gmail.com>
2025-09-06 17:48:40 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "freellearn-course-sharing",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.0",
+    "nodemailer": "^6.9.0"
+  }
+}
